48227050d18d5f936a5c39900f59aefc2aeba5c8
SourceQwerty <137706969+MisterRobotsk@users.noreply.github.com>
2023-11-15 19:21:48 +0300

    Added scan list
    Added a new beta feature - Tcp scan list and others.
    For example:
    ./pineapple_scan -sL file.txt

--- pineapple_scan/src/args.rs
+++ pineapple_scan/src/args.rs
@@ -2,7 +2,7 @@ pub mod addfile{
 	
 	use std::net::{TcpStream, UdpSocket};
 	use std::fs::File;
-	use std::io::Write;
+	use std::io::{self, BufRead, Write};
 	use std::time::Instant;
 	use chrono::{Datelike, Timelike, Local};
     	use std::process::Command;
@@ -27,16 +27,16 @@ pub mod addfile{
 	    println!("{:02}:{:02}:{:02}", hour, minute, second);
 	}
 	
-	pub fn scan_tcp(var: String){
+	pub fn scan_tcp(host: String){
 
-	    print!("Starting PineappleScan ({}): ", var);
+	    print!("Starting PineappleScan ({}): ", host);
 	    print_time();
             println!("please wait until the program finishes scanning the ports....");
             
 	    let start = Instant::now();
 
 	    for port in 1..1024{
-		let mut ip = String::from(&var);
+		let mut ip = String::from(&host);
 		ip.push_str(":");
 		ip.push_str(&port.to_string());
 		
@@ -50,9 +50,9 @@ pub mod addfile{
 	    println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}", duration);
 	}
 	
-	pub fn scan_udp(var: String){
+	pub fn scan_udp(host: String){
     
-	    print!("Starting PineappleScan ({}): ", var);
+	    print!("Starting PineappleScan ({}): ", host);
 	    print_time();
 	    println!("please wait until the program finishes scanning the ports....");
 	    
@@ -60,7 +60,7 @@ pub mod addfile{
 	    let mut count_closed_ports = 0;
 
 	    for port in 1..1024{
-		let mut ip = String::from(&var);
+		let mut ip = String::from(&host);
 		ip.push_str(":");
 		ip.push_str(&port.to_string());
 
@@ -112,24 +112,24 @@ pub mod addfile{
 	    println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}", duration);
 	}
 	
-	pub fn scan_record_tcp(var: String){
+	pub fn scan_record_tcp(host: String){
 
 	    let path = "dataTcp.txt";
-	    let mut output = File::create(path).unwrap();
+	    let mut output = File::create(path).expect("Please enter name file try again or enter --help");
 	    
-	    print!("Starting scan ports ({}): ", var);
+	    print!("Starting scan ports ({}): ", host);
 	    print_time();
 	    println!("please wait until the program finishes scanning the ports....");
 	    
 	    let start = Instant::now();
 
 	    for port in 1..65536{
-		let mut ip = String::from(&var);
+		let mut ip = String::from(&host);
 		ip.push_str(":");
 		ip.push_str(&port.to_string());
 
 		if let Ok(_) = TcpStream::connect(&ip){
-		    write!(output, "Port open: {}\n", &ip).unwrap();
+		    write!(output, "Port open: {}\n", &ip).expect("I don't write data to file, Sorry :(");
 		}
 	    }
 	    let duration = start.elapsed();
@@ -138,21 +138,21 @@ pub mod addfile{
 	    println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}. Record data in dataTcp.txt", duration);
 	}
 	
-	pub fn scan_record_udp(var: String){
+	pub fn scan_record_udp(host: String){
 
 	    let mut count_closed_ports = 0;
 
 	    let path = "dataUdp.txt";
-	    let mut output = File::create(path).unwrap();
+	    let mut output = File::create(path).expect("Please enter name file try again or enter --help");
 	    
-	    print!("Starting scan ports ({}): ", var);
+	    print!("Starting scan ports ({}): ", host);
 	    print_time();
 	    println!("please wait until the program finishes scanning the ports....");
 	    
 	    let start = Instant::now();
 
 	    for port in 1..1024{
-		let mut ip = String::from(&var);
+		let mut ip = String::from(&host);
 		ip.push_str(":");
 		ip.push_str(&port.to_string());
 
@@ -161,14 +161,14 @@ pub mod addfile{
 		    if let  Ok(_) = socket.connect(&ip){
 		        continue;
 		    } else {
-		        write!(output, "Port {} is closed.\n", &ip).unwrap();
+		        write!(output, "Port {} is closed.\n", &ip).expect("I dont't write data to file, Sorry :(");
 		        count_closed_ports += 1;
 		    }
 		}
 	    }
 	    
 	    if count_closed_ports == 0{
-		write!(output, "All port is free from 1 before 1023").unwrap();
+		write!(output, "All port is free from 1 before 1023").expect("I don't write data to file, Sorry :(");
 	    }
 	    let duration = start.elapsed();
 	    print!("\nCompleted PineappleScan: ");
@@ -176,9 +176,9 @@ pub mod addfile{
 	    println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}. Record data in dataUdp.txt", duration);
 	}
 
-    pub fn scan_ping(var: String){
+    pub fn scan_ping(host: String){
         
-        print!("Starting scan ping port ({}): ", var);
+        print!("Starting scan ping port ({}): ", host);
         print_time();
         println!("please wait until the program finishes scanning the ports....");
 
@@ -190,7 +190,7 @@ pub mod addfile{
                                 .arg("1")
                                 .arg("-w")
                                 .arg("1")
-                                .arg(&var)
+                                .arg(&host)
                                 .output()
                                 .expect("Failed to execute command");
             if output.status.success(){
@@ -206,12 +206,12 @@ pub mod addfile{
         println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}", duration);
     }
 
-    pub fn scan_record_ping(var: String){
+    pub fn scan_record_ping(host: String){
         
         let path = "dataPing.txt";
-        let mut file_output = File::create(path).unwrap();
+        let mut file_output = File::create(path).expect("Please enter name file try again or enter --help");
     
-        print!("Starting scan ping port ({}): ", var);
+        print!("Starting scan ping port ({}): ", host);
         print_time();
         println!("please wait until the program finishes scanning the ports....");
         
@@ -223,14 +223,14 @@ pub mod addfile{
                                 .arg("1")
                                 .arg("-w")
                                 .arg("1")
-                                .arg(&var)
+                                .arg(&host)
                                 .output()
                                 .expect("Failed to execute command");
 
             if output.status.success(){
                 continue;
             } else {
-                write!(file_output, "Port {} is closed.\n", &port).unwrap();
+                write!(file_output, "Port {} is closed.\n", &port).expect("I don't write data to file, Sorry :(");
             }
         }
 
@@ -240,22 +240,22 @@ pub mod addfile{
         println!("PineappleScan done: 1 IP Address (1 host up) scanned in {:?}", duration);
     }
 
-    pub fn scan_dns(var: String){
+    pub fn scan_dns(host: String){
         
-        print!("Starting search dns ({}): ", var);
+        print!("Starting search dns ({}): ", host);
         print_time();
         println!("please wait until the program finishes scanning the ports....");
         
         let start = Instant::now();
         
-         match lookup_host(&var){
+         match lookup_host(&host){
             Ok(ip_addresses) => {
                 for ip in ip_addresses{
-                    println!("Name:   {}\nAddress: {}", var, ip);
+                    println!("Name:   {}\nAddress: {}", host, ip);
                 }
             }, 
             Err(err) => {
-                println!("Failed finish work\nName:   {}\nAddress: {}", var, err)
+                println!("Not connecting to the server\nName:   {}\nAddress: {}", host, err)
             }
          }
          
@@ -265,25 +265,25 @@ pub mod addfile{
          println!("PineappleScan done: 1 IP Address (1 host up) search dns in {:?}", duration);
     }
 
-    pub fn scan_record_dns(var: String){
+    pub fn scan_record_dns(host: String){
 
         let path = String::from("dataDns.txt");
-        let mut output = File::create(path).unwrap();
+        let mut output = File::create(path).expect("Please enter name file try again or enter --help");
 
-        print!("Starting search dns ({}): ", var);
+        print!("Starting search dns ({}): ", host);
         print_time();
         println!("please wait until the program finishes scanning the ports....");
         
         let start = Instant::now();
 
-        match lookup_host(&var){
+        match lookup_host(&host){
             Ok(ip_addresses) => {
                 for ip in ip_addresses{
-                    write!(output, "Name:   {}\nAddress: {}\n", var, ip).unwrap();
+                    write!(output, "Name:   {}\nAddress: {}\n", host, ip).expect("I don't write data to file, Sorry :(");
                 }
             },
             Err(err) => {
-                println!("Failed finish work\nName:   {}\nAddress: {}", var, err);
+                println!("Not connecting to the server\nName:   {}\nAddress: {}", host, err);
             }
         }
 
@@ -292,4 +292,27 @@ pub mod addfile{
         print_time();
         println!("PineappleScan done: 1 IP Address (1 host up) search dns in {:?}", duration);
     }
+
+	pub fn scan_list(path: String, choice: String){
+		let file = File::open(&path).expect("Please enter name file try again or enter --help");
+		let reader = io::BufReader::new(file);
+		
+		let mut first_line = String::new();
+		if let Some(Ok(line)) = reader.lines().next(){
+			first_line = line;
+		}
+
+		match choice.as_str(){
+			"tcp" => scan_tcp(first_line),
+			"udp" => scan_udp(first_line),
+			"ping" => scan_ping(first_line),
+			"dns" => scan_dns(first_line),
+
+			"record_tcp" => scan_record_tcp(first_line),
+			"record_udp" => scan_record_udp(first_line),
+			"scan_record_ping" => scan_record_ping(first_line),
+			"scan_record_dns" => scan_record_dns(first_line),
+			_ => println!("nothing!"),
+		}
+	}
 }
--- pineapple_scan/src/main.rs
+++ pineapple_scan/src/main.rs
@@ -13,19 +13,33 @@ fn main(){
             Command:
                 -h or --help                print all command program
                 -v or --version             print version command
-                -sT or --scanTcp            show open ports using TCP scan
+                
+		-sT or --scanTcp            show open ports using TCP connect scan
                 -sU or --scanUdp            
                 -sP or --scanPing           shows open ports using ping scan
-                -sRP or --scanRecordPing    records open ports using ping scan in file
-                -sRT or --scanRecordTcp     records open ports using TCP scan in file
+                
+		-sRP or --scanRecordPing    records open ports using ping scan in file
+                -sRT or --scanRecordTcp     records open ports using TCP connect scan in file
                 -sRU or --scanRecordUdp     records open ports using UDP scan in file
-                -sD or --scanDns            shows the DNS of the host
+                
+		-sD or --scanDns            shows the DNS of the host
                 -sRD or --scanRecordDns     records the DNS of the host in file
+		
+		-sLT or --scanListTcp	    scans the list of ports using tcp
+		-sLU or --scanListUdp	    scans the list of ports using udp
+		-sLP or --scanListPing	    scans the list of ports using ping 
+		-sLD or --scnaListDns	    uses the list to show DNS of the host 
+
+		-sLRT or --scanListRecordTcp	scans the list of ports using TCP connect and writes open ports to a file
+		-sLRU or --scanListRecordUdp	scans the list of ports using UDP and writes open ports to a file
+		-sLRP or --scanListRecordPing	scans the list of ports using Ping and writes open ports to a file
+		-sLRD or --scanListRecordDns	using the list writes to a file DNS of the host
             
             Example:
                 -sT 127.0.0.1
                 --scanPing example.com
                 -sD example.com
+		-sLU file.txt
 
                 [ARG] <INPUT>
                 ".to_string();
@@ -39,15 +53,23 @@ fn main(){
 
         match command.as_str(){
             "-h" | "--help" => println!("{}", help),
-            "-v" | "--version" => println!("Version: 1.0"),
-            "-sT" | "--scanTcp" => scan_tcp(args[2].to_string()),
+            "-v" | "--version" => println!("Version: 1.1"),
+            
+	    "-sT" | "--scanTcp" => scan_tcp(args[2].to_string()),
             "-sU" | "--scanUdp" => scan_udp(args[2].to_string()),
-            "-sRT" | "--scanRecordTcp" => scan_record_tcp(args[2].to_string()),
+            
+	    "-sRT" | "--scanRecordTcp" => scan_record_tcp(args[2].to_string()),
             "-sRU" | "--scanRecordUdp" => scan_record_udp(args[2].to_string()),
-            "-sP" | "--scanPing" => scan_ping(args[2].to_string()),
-            "-sRP" | "--scanRecordPing" => scan_record_ping(args[2].to_string()),
-            "-sD" | "--scanDns" => scan_dns(args[2].to_string()),
+            "-sP"  | "--scanPing" => scan_ping(args[2].to_string()),
+            
+	    "-sRP" | "--scanRecordPing" => scan_record_ping(args[2].to_string()),
+            "-sD"  | "--scanDns" => scan_dns(args[2].to_string()),
             "-sRD" | "--scanRecordDns" => scan_record_dns(args[2].to_string()),
+
+	    "-sLT" | "--scanListTcp" => scan_list(args[2].to_string(), "tcp".to_string()),
+	    "-sLU" | "--scanListUdp" => scan_list(args[2].to_string(), "udp".to_string()),
+	    "-sLP" | "--scanListPing" => scan_list(args[2].to_string(), "ping".to_string()),
+	    "-sLD" | "--scanListDns" => scan_list(args[2].to_string(), "dns".to_string()), 
             _ => println!("please use command -h or --help"),
         }
     } else {
